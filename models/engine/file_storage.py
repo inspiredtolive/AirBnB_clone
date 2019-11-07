0ead3ebcff891988db207e084b90b9c88014dc34
Marco Chan <inspiredtolive0@gmail.com>
2019-11-07 21:05:05 +0000

    Uses class attribute __objects

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -16,12 +16,12 @@ class FileStorage:
     def new(self, obj):
         """Sets in __objects the obj with key <obj class name>.id."""
         key = obj.__class__.__name__ + "." + obj.id
-        self.__objects[key] = obj.to_dict()
+        FileStorage.__objects[key] = obj.to_dict()
 
     def save(self):
         """Serializes __objects to the JSON file (__file_path)."""
         with open(FileStorage.__file_path, "w") as f:
-            json.dump(self.__objects, f)
+            json.dump(FileStorage.__objects, f)
 
     def reload(self):
         """Deserializes the JSON file (__file_path) to __objects."""
