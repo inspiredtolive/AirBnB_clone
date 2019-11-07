75fd56ff542596918284f3de7f0ca269eba05aae
Marco Chan <inspiredtolive0@gmail.com>
2019-11-07 02:55:38 +0000

    Creates BaseModel from a dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -1,28 +1,37 @@
 #!/usr/bin/python3
 """Defines all attributes and methods for BaseModel"""
 import uuid
-import datetime
+from datetime import datetime
+
 
 class BaseModel:
     """BaseModel class"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        if kwargs:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    setattr(self, k, datetime.strptime(v,
+                            "%Y-%m-%dT%H:%M:%S.%f"))
+                elif k != "__class__":
+                    setattr(self, k, v)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Return string format of instance"""
-        return "[{}] ({}) <{}>".format(self.__class__.__name__, self.id,\
+        return "[{}] ({}) <{}>".format(self.__class__.__name__, self.id,
                                        self.__dict__)
 
     def save(self):
         """Update updated_at attribute with current time"""
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
-        """Return dictionary containing all key/value of __dict__ of instance"""
+        """Returns a dict containing all key/value of __dict__ of instance"""
         new_dict = self.__dict__.copy()
         new_dict["__class__"] = self.__class__.__name__
         new_dict["created_at"] = self.created_at.isoformat()
