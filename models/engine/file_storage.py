8fda78567f0051524633b1f5b75f0bebe3cad11c
Marco Chan <inspiredtolive0@gmail.com>
2019-11-08 13:30:58 -0800

    Correctly saves and reloads all attribute of the instances

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,7 +2,7 @@
 """Defines a FileStorage class."""
 import json
 from pathlib import Path
-
+from models.base_model import BaseModel
 
 class FileStorage:
     """A class for object serialization using a JSON file."""
@@ -16,16 +16,17 @@ class FileStorage:
     def new(self, obj):
         """Sets in __objects the obj with key <obj class name>.id."""
         key = obj.__class__.__name__ + "." + obj.id
-        FileStorage.__objects[key] = obj.to_dict()
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """Serializes __objects to the JSON file (__file_path)."""
+        my_dict = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
         with open(FileStorage.__file_path, "w") as f:
-            json.dump(FileStorage.__objects, f)
+            json.dump(my_dict, f)
 
     def reload(self):
         """Deserializes the JSON file (__file_path) to __objects."""
         config = Path(FileStorage.__file_path)
         if config.is_file():
             with open(FileStorage.__file_path, "r") as f:
-                FileStorage.__objects = json.load(f)
+                FileStorage.__objects = {k: BaseModel(**v) for k, v in json.load(f).items()}
