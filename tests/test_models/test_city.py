14bcda728e3ef29f9c8a1ba1604fa4ee3f7bb0fb
Marco Chan <inspiredtolive0@gmail.com>
2019-11-13 19:13:46 -0800

    Tests for Basemodel inheritance

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -7,21 +7,19 @@ from models.city import City
 
 class TestCity(unittest.TestCase):
     """A TestCase for City class."""
-    def test_state_id_exists(self):
-        """Tests the default state_id value."""
-        instance = City()
-        self.assertEqual(hasattr(instance, "state_id"), True)
-        self.assertIs(type(instance.state_id), str)
-        self.assertEqual(instance.state_id, "")
-
-    def test_name_exists(self):
-        """Tests the default name value."""
-        instance = City()
-        self.assertEqual(hasattr(instance, "name"), True)
-        self.assertIs(type(instance.name), str)
-        self.assertEqual(instance.name, "")
-
     def test_inherits_BaseModel(self):
         """Tests if City inherits from BaseModel."""
         instance = City()
         self.assertIsInstance(instance, BaseModel)
+
+    def test_state_id_exists(self):
+        """Tests the default state_id value."""
+        self.assertEqual(hasattr(City, "state_id"), True)
+        self.assertIs(type(City.state_id), str)
+        self.assertEqual(City.state_id, "")
+
+    def test_name_exists(self):
+        """Tests the default name value."""
+        self.assertEqual(hasattr(City, "name"), True)
+        self.assertIs(type(City.name), str)
+        self.assertEqual(City.name, "")
